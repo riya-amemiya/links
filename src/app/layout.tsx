27074d771408fb2ceaa81a9982aa38d0fdfcf989
Yuta Nishi <riyaamemiya@gmail.com>
2023-11-15 17:30:02 +0900

    refactor(layout): メタデータオブジェクトの型定義追加と拡張

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,40 @@
 import "@/styles/globals.css";
+import { Metadata } from "next";
 import "the-new-css-reset/css/reset.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Amemiya Riya Links",
   description: "Amemiya Riya Links",
+  alternates: {
+    canonical: defaultUrl,
+  },
+  authors: {
+    name: "Amemiya Riya",
+    url: "https://twitter.com/AmemiyaRiya",
+  },
+  robots: {
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  openGraph: {
+    title: "Amemiya Riya Links",
+    description: "Amemiya Riya Links",
+    url: defaultUrl,
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@AmemiyaRiya",
+    title: "Amemiya Riya Links",
+    description: "Amemiya Riya Links",
+  },
 };
 
 export default function RootLayout({
