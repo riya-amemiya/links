08aae565a091cb7ce1a430c5717e81cc3b65ddf6
Riya Amemiya <riyaamemiya@gmail.com>
2023-11-16 00:14:04 +0900

    refactor(app): アイコンの扱いを変更し、コードのリファクタリングを行いました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,43 +7,45 @@ import {
   PersonIcon,
   TwitterLogoIcon,
 } from "@radix-ui/react-icons";
+import type { IconProps } from "@radix-ui/react-icons/dist/types";
+import type { ForwardRefExoticComponent, RefAttributes } from "react";
 async function Index() {
   const links: {
     name: string;
     url: string;
-    icon: JSX.Element;
+    Icon: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>;
   }[] = [
     {
       name: "Portfolio",
       url: "https://lapras.com/public/riyaamemiya",
-      icon: <PersonIcon className="w-5 h-5 mr-4" />,
+      Icon: PersonIcon,
     },
     {
       name: "Twitter",
       url: "https://twitter.com/Riya31377928",
-      icon: <TwitterLogoIcon className="w-5 h-5 mr-4" />,
+      Icon: TwitterLogoIcon,
     },
     {
       name: "GitHub",
       url: "https://github.com/riya-amemiya",
-      icon: <GitHubLogoIcon className="w-5 h-5 mr-4" />,
+      Icon: GitHubLogoIcon,
     },
     {
       name: "Diary",
       url: "https://sizu.me/riya_amemiya",
-      icon: <Link2Icon className="w-5 h-5 mr-4" />,
+      Icon: Link2Icon,
     },
     {
       name: "Zenn",
       url: "https://zenn.dev/riya_amemiya",
-      icon: <Link2Icon className="w-5 h-5 mr-4" />,
+      Icon: Link2Icon,
     },
   ];
   return (
     // 真ん中に表示する
     <Card className="max-w-md mx-auto bg-slate-50 rounded-xl shadow-md md:max-w-2xl overflow-hidden w-full p-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:mt-0 mt-10">
       <div className="flex-1 w-full flex flex-col items-center">
-        <div className="md:flex-shrink-0 gap-4">
+        <div className="md:flex-shrink-0">
           <Avatar className="h-full w-full md:w-48">
             <AvatarImage alt="User avatar" src="/icon.png" />
             <AvatarFallback>ICON</AvatarFallback>
@@ -61,19 +63,23 @@ async function Index() {
             趣味としてプログラミングを楽しんでいます。
           </p>
           <ul className="mt-4 space-y-4">
-            {links.map((link) => (
-              <li className="flex items-center space-x-2 w-36" key={link.url}>
+            {links.map(({ url, name, Icon }) => (
+              <li className="flex items-center space-x-2 w-36" key={url}>
                 <Button className="w-full">
                   <div className="relative w-full">
                     <div className="w-full flex items-center justify-center">
-                      {link.icon}
+                      <div className="w-4/12">
+                        <div className="w-full flex justify-center items-center">
+                          <Icon className="w-5 h-5" />
+                        </div>
+                      </div>
                       <a
-                        className="text-sm text-white"
-                        href={link.url}
+                        className="text-sm text-white w-8/12 text-center"
+                        href={url}
                         target="_blank"
                         rel="noopener noreferrer"
                       >
-                        {link.name}
+                        {name}
                       </a>
                     </div>
                   </div>
