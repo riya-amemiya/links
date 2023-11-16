6ed6f6fa1a37b6ce093786d731598b8c3e060586
Yuta Nishi <riyaamemiya@gmail.com>
2023-11-16 17:01:04 +0900

    refactor: READMEとページコンポーネントのリファクタリングおよび構成管理の導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,10 @@
-import {
-  GitHubLogoIcon,
-  Link2Icon,
-  PersonIcon,
-  TwitterLogoIcon,
-} from "@radix-ui/react-icons";
-import type { IconProps } from "@radix-ui/react-icons/dist/types";
 import Image from "next/image";
-import type { ForwardRefExoticComponent, RefAttributes } from "react";
 
 import { Card } from "@/components/ui/card";
 import { LinkButton } from "@/components/ui/linkButton";
+import { config } from "@/config";
 
 async function Index() {
-  const links: {
-    name: string;
-    url: string;
-    Icon: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>;
-  }[] = [
-    {
-      name: "Portfolio",
-      url: "https://lapras.com/public/riyaamemiya",
-      Icon: PersonIcon,
-    },
-    {
-      name: "Twitter",
-      url: "https://twitter.com/Riya31377928",
-      Icon: TwitterLogoIcon,
-    },
-    {
-      name: "GitHub",
-      url: "https://github.com/riya-amemiya",
-      Icon: GitHubLogoIcon,
-    },
-    {
-      name: "Diary",
-      url: "https://sizu.me/riya_amemiya",
-      Icon: Link2Icon,
-    },
-    {
-      name: "Zenn",
-      url: "https://zenn.dev/riya_amemiya",
-      Icon: Link2Icon,
-    },
-  ];
   return (
     // 真ん中に表示する
     <Card className="max-w-md mx-auto bg-slate-50 rounded-xl shadow-md md:max-w-2xl overflow-hidden w-full p-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:mt-0 mt-10">
@@ -53,24 +15,22 @@ async function Index() {
               alt="User avatar"
               height={500}
               loading="eager"
-              src="/icon.png"
+              src={config.iconImage}
               width={500}
             />
           </div>
         </div>
         <div className="flex-1 w-full flex flex-col items-center">
           <h1 className="block mt-1 text-2xl leading-tight font-medium text-black">
-            Amemiya Riya
+            {config.name}
           </h1>
-          <p className="mt-2 text-gray-500 text-center text-base">
-            最近はReactずっと書いてます。
-            <br />
-            TSとPythonが得意です。
-            <br />
-            趣味としてプログラミングを楽しんでいます。
-          </p>
+          {config.description && (
+            <p className="mt-2 text-gray-500 text-center text-base">
+              {config.description}
+            </p>
+          )}
           <ul className="mt-4 space-y-4">
-            {links.map(({ url, name, Icon }) => (
+            {config.links.map(({ url, name, Icon }) => (
               <li
                 className="flex items-center space-x-2 w-36 animate__animated animate__fadeInUp"
                 key={url}
