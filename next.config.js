aa39d39aa3906b30b207f0f4b4c53f72e550b52f
Riya Amemiya <riyaamemiya@gmail.com>
2023-11-25 10:59:48 +0900

    chore(next.config.js): Content-Security-Policy の connect-src を更新

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ const nextConfig = {
               {
                 key: "Content-Security-Policy",
                 value:
-                  "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' static.cloudflareinsights.com *.pagespeed-mod.com *.google-analytics.com *.googletagmanager.com; img-src * data:; style-src 'self' 'unsafe-inline'; connect-src 'https://www.google-analytics.com';",
+                  "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' static.cloudflareinsights.com *.pagespeed-mod.com *.google-analytics.com *.googletagmanager.com; img-src * data:; style-src 'self' 'unsafe-inline'; connect-src 'https://www.google-analytics.com' 'https://www.google-analytics.com';",
               },
               {
                 key: "Permissions-Policy",
