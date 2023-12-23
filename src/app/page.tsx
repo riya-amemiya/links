4919abebac58194e34144023f661ca654bf386a9
Riya Amemiya <riyaamemiya@gmail.com>
2023-12-23 22:51:00 +0900

    style: .eslintignoreとpage.tsxのスタイルを調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ const Index = async () => {
   const data = await getMicrocms<Config>("config");
   return (
     // 真ん中に表示する
-    <Card className="max-w-md mx-auto bg-slate-50 rounded-xl shadow-md md:max-w-2xl overflow-scroll w-full p-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:mt-0 mt-10">
+    <Card className="max-w-md mx-auto bg-slate-50 rounded-xl shadow-md md:max-w-2xl w-full md:p-10 pb-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-md:mt-10 max-sm:mt-20">
       <div className="flex-1 w-full flex flex-col items-center">
         <div className="md:flex-shrink-0">
-          <Avatar asChild={true} className="h-full w-full md:w-48">
+          <Avatar asChild={true} className="h-full w-full md:w-64">
             <Image
               alt="User avatar"
               height={500}
@@ -29,7 +29,7 @@ const Index = async () => {
           <h1 className="block mt-1 text-2xl leading-tight font-medium text-black">
             {data.name}
           </h1>
-          <div className="mt-2 text-gray-500 text-center text-base">
+          <div className="mt-2 text-gray-500 text-center text-base md:text-lg">
             {htmr(data.description)}
           </div>
           <ul className="mt-4 flex flex-wrap justify-center items-center gap-4">
