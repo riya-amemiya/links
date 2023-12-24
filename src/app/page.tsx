a3b21443c2722c63653db868a474f42d321f90b9
Riya Amemiya <riyaamemiya@gmail.com>
2023-12-24 22:25:05 +0900

    refactor(components): リンクリストを新しい`Links`コンポーネントに分割

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import htmr from "htmr";
 import Image from "next/image";
 import { QRCodeSVG } from "qrcode.react";
 
+import { Links } from "@/components/home/links";
 import { Avatar } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
@@ -14,7 +15,6 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { defaultUrl } from "@/config/defaultUrl";
-import { iconData } from "@/config/iconData";
 import { getMicrocms } from "@/lib/getMicrocms";
 import { Config } from "@/types/configType";
 const Index = async () => {
@@ -61,34 +61,7 @@ const Index = async () => {
             <div className="mt-2 text-gray-500 text-center text-base md:text-lg">
               {htmr(data.description)}
             </div>
-            <ul className="mt-4 flex flex-wrap justify-center items-center gap-4">
-              {data.links.reverse().map(({ url, name, icon }) => {
-                const Icon = iconData[icon[0]];
-                return (
-                  <li
-                    className="flex items-center w-36 animate__animated animate__fadeInUp"
-                    key={url}
-                  >
-                    <Button asChild={true} className="w-full">
-                      <a href={url} rel="noopener noreferrer" target="_blank">
-                        <div className="relative w-full">
-                          <div className="w-full flex items-center justify-center">
-                            <div className="w-4/12">
-                              <div className="w-full flex justify-center items-center">
-                                <Icon className="w-5 h-5" />
-                              </div>
-                            </div>
-                            <span className="text-sm text-white w-8/12 text-center">
-                              {name}
-                            </span>
-                          </div>
-                        </div>
-                      </a>
-                    </Button>
-                  </li>
-                );
-              })}
-            </ul>
+            <Links data={data} />
           </div>
         </div>
       </CardContent>
---
+++ src/components/home/links.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { iconData } from "@/config/iconData";
+import { Config } from "@/types/configType";
+
+export const Links = ({ data }: { data: Config }) => {
+  return (
+    <>
+      <ul className="mt-4 flex flex-wrap justify-center items-center gap-4">
+        {data.links.reverse().map(({ url, name, icon }) => {
+          const Icon = iconData[icon[0]];
+          return (
+            <li
+              className="flex items-center w-36 animate__animated animate__fadeInUp"
+              key={url}
+            >
+              <Button asChild={true} className="w-full">
+                <a href={url} rel="noopener noreferrer" target="_blank">
+                  <div className="relative w-full">
+                    <div className="w-full flex items-center justify-center">
+                      <div className="w-4/12">
+                        <div className="w-full flex justify-center items-center">
+                          <Icon className="w-5 h-5" />
+                        </div>
+                      </div>
+                      <span className="text-sm text-white w-8/12 text-center">
+                        {name}
+                      </span>
+                    </div>
+                  </div>
+                </a>
+              </Button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
