99e6b22210408dbbded305c25e2bbe0ad607b5c0
Riya Amemiya <riyaamemiya@gmail.com>
2023-12-31 17:27:01 +0900

    feat(manifest): マニフェストとロボット設定を追加し、OGとTwitterの画像を追加

---
+++ src/app/manifest.ts
@@ -0,0 +1,35 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    short_name: "Riya Links",
+    name: "Amemiya Riya Links",
+    icons: [
+      {
+        src: "/icons/icon-192x192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        src: "/icons/icon-256x256.png",
+        type: "image/png",
+        sizes: "256x256",
+      },
+      {
+        src: "/icons/icon-384x384.png",
+        type: "image/png",
+        sizes: "384x384",
+      },
+      {
+        src: "/icons/icon-512x512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+    start_url: ".",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    theme_color: "#000000",
+  };
+}
---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+import { defaultUrl } from "@/config/defaultUrl";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${defaultUrl}/sitemap.xml`,
+  };
+}
