d00dec507519bf760d612c04b10c77dc35562373
Riya Amemiya <riyaamemiya@gmail.com>
2024-01-08 22:30:49 +0900

    perf(next.config.js): 依存関係のトレースから不要な除外設定を削除

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    outputFileTracingExcludes: {
-      "*": [
-        "node_modules/@swc/core-linux-x64-gnu",
-        "node_modules/@swc/core-linux-x64-musl",
-        "node_modules/@esbuild/linux-x64",
-      ],
-    },
-    webpackBuildWorker: true,
-  },
   images: {
     remotePatterns: [
       {
