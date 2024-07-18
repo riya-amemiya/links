18927108b2e78300f3ce0e9bb45f721f02ab852b
Yuta Nishi <riyaamemiya@gmail.com>
2024-07-18 16:08:38 +0900

    refactor(microcmsClient): 型アサーションを追加して環境変数の型を明確化

--- src/lib/microcmsClient.ts
+++ src/lib/microcmsClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const microcmsClient = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICEDOMAIN || "",
-  apiKey: process.env.MICROCMS_API_KEY || "",
+  serviceDomain: process.env.MICROCMS_SERVICEDOMAIN as string,
+  apiKey: process.env.MICROCMS_API_KEY as string,
 });
