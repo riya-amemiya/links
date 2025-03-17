cbd037a63e18aa8216161a045229e95f528802c7
Yuta Nishi <riyaamemiya@gmail.com>
2025-03-17 22:19:56 +0900

    refactor: vitest設定ファイルを整理し、experimental-nextjs-viteを使用するように変更

---
+++ vitest.config.ts
@@ -0,0 +1,36 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
+import { defineConfig } from "vitest/config";
+
+const dirname =
+  import.meta.dirname === undefined
+    ? path.dirname(fileURLToPath(import.meta.url))
+    : import.meta.dirname;
+
+// More info at: https://storybook.js.org/docs/writing-tests/test-addon
+export default defineConfig({
+  test: {
+    workspace: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+          storybookTest({ configDir: path.join(dirname, ".storybook") }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
+  },
+});
