803bfd27afc625497aceaff23ddf1015a7bd55ea
Riya Amemiya <riya-amemiya+github@tokidux.com>
2026-06-11 13:18:20 +0900

    feat: remove boot splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import { BootGate } from "@/components/arcade/boot-gate";
 import { CharacterSelect } from "@/components/arcade/character-select";
 import { getMicrocms } from "@/lib/getMicrocms";
 
 const Index = async () => {
   const profile = await getMicrocms("profile");
-  return (
-    <BootGate name={profile.name}>
-      <CharacterSelect profile={profile} />
-    </BootGate>
-  );
+  return <CharacterSelect profile={profile} />;
 };
 export default Index;
